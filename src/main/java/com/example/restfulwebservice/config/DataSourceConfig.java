98ac1da18f3702d35cf4318179d903034ba27eed
Statera <bom3632@gmail.com>
2022-01-02 18:40:52 +0900

    data source 1개로 수정

--- src/main/java/com/example/restfulwebservice/config/DataSourceConfig.java
+++ src/main/java/com/example/restfulwebservice/config/DataSourceConfig.java
@@ -9,9 +9,9 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    @ConfigurationProperties(prefix = "spring.datasource")
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    @Bean
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
 }
